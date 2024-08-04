911f9b0954f6ed38a3bf232fafaa4da1520ef142
Kang The Variants <78076796+khengyun@users.noreply.github.com>
2024-08-04 14:11:06 +0700

    add Vietnamese

--- src/routes/llama/+server.js
+++ src/routes/llama/+server.js
@@ -120,6 +120,9 @@ export async function POST({ request, platform }) {
 		case 'france':
 			prompt = `fais une courte et cruelle critique sarcastique en argot pour le profil GitHub suivant : ${username}. Voici les détails : "${JSON.stringify(datas)}"`;
 			break;
+		case 'vietnamese':
+			prompt = `Hãy đưa ra một lời châm chọc ngắn gọn và tàn nhẫn bằng tiếng lóng cho hồ sơ GitHub sau: ${username}. Đây là chi tiết: "${JSON.stringify(datas)}"`;
+			break;
 	}
 
 	// answerdebug += prompt + '\n';
