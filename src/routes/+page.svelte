64149de237694923a3a4a087da88edcd79db70a1
style77 <stylek777@gmail.com>
2024-08-04 02:07:47 +0200

    Add Polish

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,7 +16,8 @@
 		{ value: 'indian', label: 'Hindi' },
 		{ value: 'korean', label: 'Korean' },
 		{ value: 'japanese', label: 'Japanese' },
-		{ value: 'chinese', label: 'Chinese' }
+		{ value: 'chinese', label: 'Chinese' },
+		{ value: 'polish', label: 'Polish' }
 	];
 
 	onMount(() => {
--- src/routes/llama/+server.js
+++ src/routes/llama/+server.js
@@ -120,6 +120,9 @@ export async function POST({ request, platform }) {
 		case 'france':
 			prompt = `fais une courte et cruelle critique sarcastique en argot pour le profil GitHub suivant : ${username}. Voici les détails : "${JSON.stringify(datas)}"`;
 			break;
+		case 'polish':
+			prompt = `krótko i ostro skrytykuj poniższy profil GitHub: ${username}. Oto szczegóły: "${JSON.stringify(datas)}"`;
+			break;
 	}
 
 	// answerdebug += prompt + '\n';
